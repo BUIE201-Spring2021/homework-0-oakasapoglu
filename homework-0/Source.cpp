687212c92350848cdb27385e0335f361d5404549
oakas <oakas@DESKTOP-4D6EQKA>
2021-03-30 17:26:01 +0300

    Commit 2  : Fibonacci

--- homework-0/Source.cpp
+++ homework-0/Source.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "My name is Omer" << endl;
 
+int Area(int x){
+	return x*x;
+}
+
+int Fibonac(int x) {
+	if (x < 3) {
+		return 1;
+	}
+	int Current = 1;
+	return Current = Fibonac(x-1) + Fibonac(x-2);
+}
+
+
+int Factorial(int x) {
+	if (x == 1 ){
+		return 1;
+	}
+	else
+	{
+		return Factorial(x-1)*x;
+	}
+}
+
+
+int main()
+{	
+	int x = 8;
+	//cin >> x;
+	//x = Factorial(x);
+	x = Fibonac(x);
+	cout << "My name is Omer : " << x << endl;
 	return 0;
 }
+
+
+
